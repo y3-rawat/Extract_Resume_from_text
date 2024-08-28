380e82bea8191cdc50f67975016ea52a27b931b7
Yash Rawat <112065069+y3-rawat@users.noreply.github.com>
2024-08-28 09:29:22 +0530

    logging

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import time 
+
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from concurrent.futures import ThreadPoolExecutor
@@ -13,27 +13,27 @@ api = None
 def index():
     return render_template('index.html')
 
-def get_resume(additional_information, extracted_text):
-    final_resume = calculations.resume_final(extracted_text, additional_information)
-    return final_resume
+# def get_resume(additional_information, extracted_text):
+#     final_resume = calculations.resume_final(extracted_text, additional_information)
+#     return final_resume
 
 @app.route('/submit', methods=['POST'])
 def submit():
 
-    job_description = request.args.get('job_description', '')
-    additional_information = request.args.get('additional_information', '')
-    extracted_text = request.args.get('ext-text', '')
-    api_key = request.args.get('api', '')
-    apis.API_func(api_key)
-    start_time = time.time()
+#     job_description = request.args.get('job_description', '')
+#     additional_information = request.args.get('additional_information', '')
+#     extracted_text = request.args.get('ext-text', '')
+#     api_key = request.args.get('api', '')
+#     apis.API_func(api_key)
+#     start_time = time.time()
 
-    output = get_resume(additional_information, extracted_text)
-    end_time = time.time()
-    time_taken = end_time - start_time
-    # Print the time taken
-    print("processing Completed")
-    print(f"Time taken by get_data: {time_taken:.2f} seconds")
-
+#     output = get_resume(additional_information, extracted_text)
+#     end_time = time.time()
+#     time_taken = end_time - start_time
+#     # Print the time taken
+#     print("processing Completed")
+#     print(f"Time taken by get_data: {time_taken:.2f} seconds")
+    output = "trying"
     return jsonify(output)
  
 if __name__ == '__main__':
